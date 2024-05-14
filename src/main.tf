f6befa211afb2290ba5eb5a03d65d8f1eaa3dc03
Konstantin <kosschuk@gmail.com>
2024-05-14 21:02:26 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -7,18 +7,19 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
-
-
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = "${var.vm_web_image}"
 }
+
+### web vm
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = local.name_web
+  platform_id = "${var.vm_web_platform_id}"
+
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = "${var.vms_resources_web["cores"]}"
+    memory        = "${var.vms_resources_web["memory"]}"
+    core_fraction = "${var.vms_resources_web["core_fraction"]}"
   }
   boot_disk {
     initialize_params {
@@ -26,16 +27,46 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = "${var.vm_web_preemptible}"
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = "${var.vm_web_nat}"
   }
 
   metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    serial-port-enable = "${var.metadata["serial-port-enable"]}"
+    ssh-keys           = "${var.metadata["ssh-keys"]}"
+  }
+}
+
+### db vm
+resource "yandex_compute_instance" "platform_db" {
+  name        = local.name_db
+  platform_id = "${var.vm_db_platform_id}"
+
+  resources {
+    cores         = "${var.vms_resources_db["cores"]}"
+    memory        = "${var.vms_resources_db["memory"]}"
+    core_fraction = "${var.vms_resources_db["core_fraction"]}"
+
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = "${var.vm_db_preemptible}"
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = "${var.vm_db_nat}"
+  }
+
+  metadata = {
+    serial-port-enable = "${var.metadata["serial-port-enable"]}"
+    ssh-keys           = "${var.metadata["ssh-keys"]}"
   }
 
 }
